c5f8c7bd47b2a0e195729fc126fe5e4d8eaa678e
avienop <ibad.ali.5509@gmail.com>
2025-08-30 20:23:38 +0500

    case sebstivity

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Skills from './components/Skills.jsx';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Certificates from './components/certificates.jsx';
+import Certificates from './components/Certificates.jsx';
 import Education from './components/Education.jsx';
 import Experience from './components/Experience.jsx';
 
